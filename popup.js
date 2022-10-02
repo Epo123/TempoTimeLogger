026ef54230058a658900e37b6849451ec5784667
Erik Poolman <e.poolman@youweagency.com>
2022-10-02 12:08:12 +0200

    Remove unnecessary permissions

--- popup.js
+++ popup.js
@@ -15,13 +15,6 @@ chrome.storage.sync.get(['timeLogValue'], function (result) {
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 processButton.addEventListener("click", async () => {
-    // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    // chrome.scripting.executeScript({
-    //     target: { tabId: tab.id },
-    //     func: checkCanProcess,
-    // });
-
     processTimeLog();
 });
 
